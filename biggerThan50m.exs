623a8c12a265b91add51bcd45908421fb2475a9a
Hailong Hao <haohailong@gmail.com>
2016-12-08 18:29:45 +0800

    修改代码，使用新的思路

--- biggerThan50m.exs
+++ biggerThan50m.exs
@@ -1,22 +1,14 @@
 # 找出给定目录中 50MB 及以上的所有文件（含所有子目录），存入列表，并打印该列表。
 
-# 获取目录的路径
-
-theFolder = String.trim_trailing (
-  IO.gets "This program will help you find the paths of all the files ≥ 50MB in a specific folder. Please input the path of the folder.\n" # 输入目录路径
-) # 删掉路径后面的换行符
-
 # 检查目录路径是否有效的函数, 若无效则重新输入
 
 defmodule Validatepath do
   def check_path(theFolder) do
-    unless File.dir?(theFolder) do
-      theFolder = String.trim_trailing (
-        IO.gets "Please input a right path.\n" # 再次输入目录路径
-      )
-      check_path(theFolder)
-    else
-      theFolder # 这句我为了让函数返回新输入的路径值, 似乎能解决问题, 但感觉应该有更好的实现方式
+    case File.dir?(theFolder) do
+      true -> theFolder
+      false ->
+        theFolder = String.trim_trailing (IO.gets "Please input a right path.\n")
+        check_path(theFolder)
     end
   end
 end
@@ -24,35 +16,45 @@ end
 # 遍历目录下所有文件及文件夹（包括子目录文件）
 
 defmodule WalkDirectory do
+
+  def the_file_list(theFolder) do
+    File.ls(theFolder)
+      |> elem(1)
+      |> Enum.map(&Path.join(["#{theFolder}", "#{&1}"]))
+  end
+
   def the_file_path_list(theFolder) do
-    theDir = File.ls(theFolder)
-    theDirList = elem(theDir, 1)
-    Enum.map(theDirList, fn(fname) ->
-      fname = Path.join(["#{theFolder}", "#{fname}"])
-      if File.dir?(fname) do
-        the_file_path_list(fname)
-      else
-        fname
-      end
-    end)
+    the_file_list(theFolder)
+      |> Enum.map(
+          fn fname ->
+            case File.dir?(fname) do
+              false -> fname
+              true -> the_file_path_list(fname)
+            end
+          end
+         )
+      |> List.flatten
+  end
+
+end
+
+
+# 检查是否大于等于 50m 的函数
+
+defmodule CheckSize do
+  def is_50m_plus do
+    m = File.stat(fname)
+        |> elem(1)
+        |> Map.from_struct()
+    m[:size]/1000/1000 >= 50
   end
 end
 
-# 检查是否大于等于 50m 的函数
-is_50m_plus = fn (fname) ->
-  m = File.stat(fname)
-        |> elem(1)
-        |> Map.from_struct
-  sizeToMB = m[:size]/1024/1024
-  sizeToMB >= 50
-end
+# 获取所有文件的路径列表
 
-theList = Validatepath.check_path(theFolder) # 确定获取的是正确的路径
-            |> WalkDirectory.the_file_path_list # 获取文件列表
-            |> List.flatten # 把 List 打平
-
-find50mPlusList = for x <- theList, is_50m_plus.(x), do: x # 获取大于等于 50M 的文件列表
-
-Enum.each(find50mPlusList, fn(filePath) -> IO.puts(filePath) end) # 打印列表
-
-# EOF
+IO.gets "This program will help you find the paths of all the files ≥ 50MB in a specific folder. Please input the path of the folder.\n"
+  |> String.trim_trailing
+  |> Validatepath.check_path
+  |> WalkDirectory.the_file_path_list
+  |> Enum.filter(&CheckSize.is_50m_plus(&1))
+  |> Enum.each(&IO.puts(&1))
