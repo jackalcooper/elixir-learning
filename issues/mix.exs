39388bb4b9a5eb83a08897b5d85a41ef3663da37
Hailong Hao <haohailong@gmail.com>
2016-12-08 15:12:29 +0800

    Add JSON library poison

--- issues/mix.exs
+++ issues/mix.exs
@@ -28,7 +28,8 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.10.0"}
+      {:httpoison, "~> 0.10.0"},
+      {:poison, "~> 3.0"}
     ]
   end
 end
